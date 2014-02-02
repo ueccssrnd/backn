51641e8aafbf9aac40f595c1040802235f177074
jmramos02 <ramosjosemari@gmail.com>
2014-02-02 20:43:05 +0800

    Created CRUD for Users, Hooked up login.

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -3,6 +3,37 @@ class UserController extends BaseController{
 
 	public function createAccount(){
 		$user = new User;
+		$user = prepareUser($user);
+		//save the instance.
+		$user->save();
+		//return stupid.
+		return Response::json(array("message" => 1));
+
+	}
+	public function modifyAccount(){
+		$id = Input::get('id');
+		$user = User::find($id);
+		$user = prepareUser($user);
+		$user = User::find($id);
+		$user->status = 0;
+		$user->save();
+		return Response::json(array("message" => 1));
+	}
+	public function deleteAccount(){
+		$id = Input::get('id');
+		$user = User::find('id');
+		$user->delete();
+	}
+	public function showProfile(){
+		$id = Input::get('id');
+		$user = User::find($id);
+		return Response::json($user);
+	}
+	public function getAllUsers(){
+		$users = User::all();
+		return Response::json($users);
+	}
+	public function prepareUser($user){
 		$user->username = Input::get('username');
 		$user->password = Hash::make(Input::get('password'));
 		$user->email = Input::get('email');
@@ -12,26 +43,5 @@ class UserController extends BaseController{
 		$user->last_name = Input::get('last_name');
 		$user->role = Input::get('role');
 		$user->status = 1;
-		//save the instance.
-		$user->save();
-		//return stupid.
-		return Response::json(array("message" => 1));
-
-	}
-
-	public function modifyAccount(){
-
-	}
-
-	public function deactivateAccount(){
-
-	}
-
-	public function showProfile(){
-		
-	}
-	public function getAllUsers(){
-		$users = User::all();
-		return Response::json($users);
 	}
 }
